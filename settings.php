5264c1e9d458cb34b61cc1406a311106e1ea2fe4
pie3-14 <57256066+pie3-14@users.noreply.github.com>
2021-02-14 05:11:25 +0900

    Update settings.php

--- settings.php
+++ settings.php
@@ -3,6 +3,6 @@
     define ('timezone', 'Asia/Seoul'); //시간대
     define ('keywords', '오늘의 한마디 게시판'); //메타 태그에 들어갈 키워드
     define ('description', '오늘의 한마디 게시판'); //메타 태그에 들어갈 설명
-    define ('subject', '오늘의 한마디 게시판'); //메타태그에 들어갈 주제?
+    define ('subject', '오늘의 한마디 게시판'); //메타태그에 들어갈 주제
     define ('file', './message.txt'); //파일
 ?>
